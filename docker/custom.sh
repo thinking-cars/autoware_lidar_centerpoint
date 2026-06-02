5b3c0591aaa64b6eb37c63c9d362273e1950e670
Raphael van Kempen <vankempen@thinking-cars.de>
2026-06-02 12:22:26 +0000

    test ci

--- docker/custom.sh
+++ docker/custom.sh
@@ -2,4 +2,7 @@
 apt update && apt install -y --no-install-recommends cuda-toolkit-12-8 || true
 update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.8 128
 update-alternatives --set cuda /usr/local/cuda-12.8
+rm -rf /var/lib/apt/lists/*
 rm -r /usr/local/cuda-12.6
+
+du --max-depth=5 / | sort -r -k1,1n
