865733db4e8b3510c63665eeb0d9a074de52a991
Raphael van Kempen <vankempen@thinking-cars.de>
2026-06-02 08:00:15 +0000

    remove cuda-12.6

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,4 +1,5 @@
 # update CUDA Toolkit
-apt update && apt install -y --no-install-recommends cuda-12-8 || true
+apt update && apt install -y --no-install-recommends cuda-toolkit-12-8 || true
 update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.8 128
 update-alternatives --set cuda /usr/local/cuda-12.8
+rm -r /usr/local/cuda-12.6
