9a3535a408d82523b62b4650fe373fd0712a61fc
Raphael van Kempen <vankempen@thinking-cars.de>
2026-06-03 08:15:41 +0200

    build image locally

--- docker/custom.sh
+++ docker/custom.sh
@@ -4,5 +4,3 @@ update-alternatives --install /usr/local/cuda cuda /usr/local/cuda-12.8 128
 update-alternatives --set cuda /usr/local/cuda-12.8
 rm -rf /var/lib/apt/lists/*
 rm -r /usr/local/cuda-12.6
-
-du --max-depth=5 / | sort -r -k1,1n
